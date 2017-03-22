94eeed1d0ea76bbed8202504a7bc2e3c90e38233
Zulfiqar Junejo <zulfiqar@marketlytics.com>
2017-03-22 16:29:11 +0500

    Added session handler for device

--- src/java/org/example/websocket/DeviceWebSocketServer.java
+++ src/java/org/example/websocket/DeviceWebSocketServer.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.example.websocket;
 
-/**
- *
- * @author zulfiqar
- */
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ApplicationScoped
+@ServerEndpoint("/actions")
 public class DeviceWebSocketServer {
-    
+
+    @OnOpen
+    public void open(Session session) {
+    }
+
+    @OnClose
+    public void close(Session session) {
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+    }
+
+    @OnMessage
+    public void handleMessage(String message, Session session) {
+    }
 }
